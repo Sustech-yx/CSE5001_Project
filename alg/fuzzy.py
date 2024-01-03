f1472306267462bc23455e51c801a18c3971c5e0
Sustech-yx <11912013@mail.sustech.edu.cn>
2024-01-03 15:27:30 +0000

    Add fuzzy approach, just random shuffle the data in dim 2, i.e., data.size(1).

---
+++ alg/fuzzy.py
@@ -0,0 +1,66 @@
+import torch
+from data.dataset import MNISTDataset
+from network.CNN import CNN1
+from alg.base import BaseAlg
+from tqdm import tqdm
+from utils import reduce
+
+
+class FuzzyAlgorithm(BaseAlg):
+    def __init__(self):
+        self.model = CNN1()
+        self.criterion = torch.nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
+
+    def train(self, train_loader):
+        self.model.train()
+        for data, labels in tqdm(train_loader):
+            # data = reduce(data)
+            # print(data.shape)
+            # print(data.size(1))
+            data=data[:, torch.randperm(data.size(1)), :, :]
+
+            self.optimizer.zero_grad()
+            outputs = self.model(data)
+            loss = self.criterion(outputs, labels)
+            loss.backward()
+            self.optimizer.step()
+
+    def predict(self, val_loader):
+        self.model.eval()
+        predictions = []
+        with torch.no_grad():
+            for data, _ in val_loader:
+                # data = reduce(data)
+                outputs = self.model(data)
+                _, predicted = torch.max(outputs.data, 1)
+                predictions.extend(predicted.cpu().numpy())
+        return predictions
+
+    def evaluate(self, test_loader):
+        self.model.eval()
+        correct = 0
+        total = 0
+        wrong_pred = ([], [], [])
+        with torch.no_grad():
+            for data, labels in test_loader:
+                # data = reduce(data)
+                outputs = self.model(data)
+                _, predicted = torch.max(outputs.data, 1)
+                # print(predicted, labels)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                wrong_pred[0].append(data[predicted != labels])
+                wrong_pred[1].append(predicted[predicted != labels])
+                wrong_pred[2].append(labels[predicted != labels])
+        accuracy = correct / total
+        # print(correct, total)
+        print('Accuracy on the val set: %d %%' % (100 * correct / total))
+        return wrong_pred, accuracy
+    
+    def save(self, path):
+        torch.save(self.model, path)
+
+
+    def load(self, path):
+        self.model.load_state_dict(torch.load(path))
--- network/CNN.py
+++ network/CNN.py
@@ -61,3 +61,64 @@ class CNN(nn.Module):
         out = self.out(x)
         return out
     
+class CNN1(nn.Module):
+    def __init__(self):
+        super(CNN1, self).__init__()
+        # self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        # self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        # self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        # self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        # self.fc2 = nn.Linear(128, 10)
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=10,
+                out_channels=80,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=80,
+                out_channels=160,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=160,
+                out_channels=320,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.out = torch.nn.Sequential(
+            nn.Linear(32 * 3 * 3 * 10, 64 * 10),
+            nn.Linear(64 * 10, 64),
+            nn.Linear(64, 10),
+        )
+
+    def forward(self, x):
+        # x = self.pool(torch.relu(self.conv1(x)))
+        # x = self.pool(torch.relu(self.conv2(x)))
+        # x = x.view(-1, 64 * 7 * 7)
+        # x = torch.relu(self.fc1(x))
+        # x = self.fc2(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = x.view(x.size(0), -1)
+        out = self.out(x)
+        return out
+    
